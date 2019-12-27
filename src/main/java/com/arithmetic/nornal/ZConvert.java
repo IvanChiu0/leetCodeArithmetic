3192e9fa41b3b81ee0befbc1e1cb7c4619b7c1f6
IvanChiu0 <409237119@qq.com>
2019-12-27 14:13:31 +0800

    ZConvert

--- src/main/java/com/arithmetic/nornal/ZConvert.java
+++ src/main/java/com/arithmetic/nornal/ZConvert.java
@@ -5,9 +5,32 @@ package com.arithmetic.nornal;
  */
 public class ZConvert {
 
+    /**
+     * 思路：
+     * 先排序一下看看规律：0123456789ABCDEFGH
+     * 0    8    G
+     * 1   79   FH
+     * 2  6 A  E
+     * 3 5  B D
+     * 4    C
+     * 由上面可见（假如行数=r）
+     * 1、每一个周期都是个2r-2元素的长度
+     * 2、首行和末行是等差数列，A=10...H=17，那么这两行的差值=2r-2
+     * 3、难就难在其他行的规律，现在把上面的二维数组变一下，两边压缩一下，得出下面：
+     * 0 8 G
+     * 179FH
+     * 26AE
+     * 35BD
+     * 4 C
+     * 可以看得处理其他行也是等差数列，并且等差值依然是2r-2，例如第二行的1和9，7和F
+     * 如此解法就得出来了
+     * @param s
+     * @param numRows
+     * @return
+     */
     public static String convert(String s, int numRows) {
         int length = s.length();
-        if (length <= numRows) {
+        if (length <= numRows || numRows == 1) {
             return s;
         }
 
@@ -16,13 +39,13 @@ public class ZConvert {
             int cycleNum = numRows;
             int lines = length % numRows == 0 ? length / cycleNum : (length / cycleNum) + 1;
             StringBuilder builder = new StringBuilder();
-            for (int r = 0 ; r < numRows ; r++) {
+            for (int r = 0; r < numRows; r++) {
                 int count = 0;
-                for (int l = 0 ; l < lines ; l++) {
+                for (int l = 0; l < lines; l++) {
                     if (r + count * cycleNum >= length) {
                         continue;
                     }
-                    builder.append(s.charAt(r+count*cycleNum));
+                    builder.append(s.charAt(r + count * cycleNum));
                     count++;
                 }
             }
@@ -30,104 +53,57 @@ public class ZConvert {
         }
 
         //以下是当成3行以上的情况
-        if (numRows % 2 != 0) {
-            //奇数行的情况
-            //周期的字符个数
-            int cycleNum = numRows + 1;
-            //中间行
-            int middleRow = (numRows - 1) / 2;
-            //列数就可以得知
-            int lines = length % cycleNum == 0 ? (length / cycleNum) * 2 : (length / cycleNum) * 2 + 1;
-            //对于中间行的周期字符个数
-            int midCycleNum = numRows - middleRow;
-            StringBuilder builder = new StringBuilder();
-            for (int r = 0; r < numRows; r++) {
-                int count = 0;
-                for (int l = 0; l < lines; l+=2) {
-                    if (r != middleRow) {
-                        //假如长度超出字符个数则跳过
-                        if (r + count * cycleNum >= length) {
-                            continue;
-                        }
-                        builder.append(s.charAt(r + count * cycleNum));
-                        count++;
-                    } else {
-                        //假如长度超出字符个数则跳过
-                        if (r + count * cycleNum >= length) {
-                            continue;
-                        }
-                        if ((r + midCycleNum + count * cycleNum) >= length) {
-                            builder.append(s.charAt(r + count * cycleNum));
-                        }else {
-                            builder.append(s.charAt(r + count * cycleNum))
-                                    .append(s.charAt(r + midCycleNum + count * cycleNum));
-                        }
-                        count++;
-                    }
-                }
-            }
-            return builder.toString();
-        } else {
-            //周期字符个数
-            int cycleNum = numRows + 2;
-            //中间两行的下行
-            int downMidRow = numRows / 2;
-            //中间两行的上行
-            int upMidRow = numRows / 2 - 1;
-            //上行的周期字符个数
-            int upMidRowCycleNum = numRows - upMidRow + 1;
-            //下行的周期字符个数
-            int downMidRowCycleNum = numRows - downMidRow;
-            //列数
-            int lines = 0;
-            if (length % cycleNum == 0) {
-                lines = (length / cycleNum) * 3;
-            }else if (length % cycleNum > numRows) {
-                lines = (length /cycleNum) * 3 + 2;
-            }else {
-                lines = (length / cycleNum) * 3 + 1;
-            }
-            StringBuilder builder = new StringBuilder();
-            for (int r = 0; r < numRows; r++) {
-                int count = 0;
-                for (int l = 0; l < lines; l += 3) {
-                    if (r != downMidRow && r != upMidRow) {
-                        if ((r + count * cycleNum) > length) {
-                            continue;
-                        }
-                        builder.append(s.charAt(r + count * cycleNum));
-                        count++;
-                    } else if (r == upMidRow) {
-                        if ((r + count * cycleNum) >= length) {
-                            continue;
-                        }
-                        if ((r + upMidRowCycleNum + count * cycleNum) >= length) {
-                            builder.append(s.charAt(r + count * cycleNum));
-                        }else {
-                            builder.append(s.charAt(r + count * cycleNum))
-                                    .append(s.charAt(r + upMidRowCycleNum + count * cycleNum));
-                        }
-                        count++;
-                    } else if (r == downMidRow) {
-                        if ((r + count * cycleNum) >= length) {
-                            continue;
-                        }
-                        if ((r + downMidRowCycleNum + count * cycleNum) >= length) {
-                            builder.append(s.charAt(r + count * cycleNum));
-                        }else {
-                            builder.append(s.charAt(r + count * cycleNum))
-                                    .append(s.charAt(r + downMidRowCycleNum + count * cycleNum));
-                        }
-                        count++;
-                    }
-                }
-            }
-            return builder.toString();
+        //奇数行的情况,周期性列数必然是numRows-1
+        //周期的字符个数
+        int cycleNum = numRows + numRows - 2;
+        //列数就可以得知
+        //假如没满一个周期
+        int lines = 0;
+        if (cycleNum > length) {
+            lines = 2;
+        }else if (length % cycleNum == 0){
+            lines = (length / cycleNum) * 2;
+        }else if (length % cycleNum <= numRows) {
+            lines = (length / cycleNum) * 2 + 1;
+        }else {
+            lines = (length / cycleNum) * 2 + 2;
         }
+        StringBuilder builder = new StringBuilder();
+        for (int r = 0; r < numRows; r++) {
+            for (int l = 0; l < lines; l++) {
+                if (r == 0 || r == (numRows - 1)) {
+                    //首行和末行的情况
+                    if (l % 2 == 0) {
+                        int idx = r + (l / 2) * cycleNum;
+                        if (idx >= length) {
+                            continue;
+                        }
+                        builder.append(s.charAt(idx));
+                    }
+                } else {
+                    //中间的行
+                    //其实就是间隔列成等差数列
+                    if (l % 2 == 0) {
+                        int idx = r + (l / 2) * cycleNum;
+                        if (idx >= length) {
+                            continue;
+                        }
+                        builder.append(s.charAt(idx));
+                    } else {
+                        int idx = (cycleNum - r) + ((l - 1) / 2) * cycleNum;
+                        if (idx >= length) {
+                            continue;
+                        }
+                        builder.append(s.charAt(idx));
+                    }
+                }
+            }
+        }
+        return builder.toString();
     }
 
     public static void main(String[] args) {
-        String s = "ABCDEF";
+        String s = "PAYPALISHIRING";
         String convert = convert(s, 5);
         System.out.println(convert);
     }
